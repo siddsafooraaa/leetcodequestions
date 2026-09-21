1bb2dcb7af93683b217630d257b8066f17901d05
siddsafooraaa <siddsafooraaa@gmail.com>
2026-09-21 19:57:40 +0530

    Time: 6 ms (27.46%), Space: 47 MB (92.09%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -0,0 +1,32 @@
+class Solution {
+    int m;
+    int n;
+    int perimeter;
+    public int islandPerimeter(int[][] grid) {
+        m=grid.length;
+        n=grid[0].length;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1){
+                    dfs(grid,i,j);
+                    return perimeter;
+                }
+            }
+        }
+        return -1;
+    }
+    public void dfs(int[][] grid,int i,int j){
+        if(i<0 || i>=m || j<0 || j>=n || grid[i][j]==0){
+            perimeter++;
+            return;
+        }
+        if(grid[i][j]==-1){
+            return;
+        }
+        grid[i][j]=-1;
+        dfs(grid,i+1,j);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1);
+    }
+}
